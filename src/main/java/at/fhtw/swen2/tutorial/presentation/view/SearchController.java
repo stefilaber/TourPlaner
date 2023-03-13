7b58bda3a1c1704bfd95d8bf046081f995933cc1
stefi <if21b052@technikum-wien.at>
2023-03-13 15:11:42 +0100

    SearchController split into SearchController and PersonListController

--- src/main/java/at/fhtw/swen2/tutorial/presentation/view/SearchController.java
+++ src/main/java/at/fhtw/swen2/tutorial/presentation/view/SearchController.java
@@ -24,11 +24,10 @@ import java.util.stream.Collectors;
 //@Scope("prototype")
 @Slf4j
 public class SearchController {
+    public static final int PAGE_ITEMS_COUNT = 10;
 
     @Autowired
-    private PersonService personService;
-
-    public static final int PAGE_ITEMS_COUNT = 10;
+    private PersonListController personListController;
 
     @FXML
     private TextField searchField;
@@ -36,13 +35,6 @@ public class SearchController {
     private Button searchButton;
     @FXML
     private Label searchLabel;
-    @FXML
-    private TableView tableView = new TableView<>();
-    @FXML
-    private VBox dataContainer;
-
-    @Autowired
-    private PersonListViewModel personListViewModel;
 
     //private ObservableList<NewPersonViewModel> masterData = FXCollections.observableArrayList();
     //private ObservableList<NewPersonViewModel> results = FXCollections.observableList(masterData);
@@ -54,9 +46,9 @@ public class SearchController {
 
     @FXML
     private void initialize() {
-
         // search panel
         searchButton.setText("Search");
+
         searchButton.setOnAction(event -> loadData());
         searchButton.setStyle("-fx-background-color: slateblue; -fx-text-fill: white;");
 
@@ -69,58 +61,12 @@ public class SearchController {
         searchField.textProperty().addListener((observable, oldValue, newValue) -> {
             searchLabel.setText(newValue);
         });
-
-        initTable();
-
-
-    }
-
-    private void initTable() {
-        personService.getPersonList().forEach(p -> {
-            personListViewModel.addItem(p);
-        });
-        tableView.setItems(personListViewModel.getPersonListItems());
-        tableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
-
-        TableColumn id = new TableColumn("ID");
-        id.setCellValueFactory(new PropertyValueFactory("id"));
-        TableColumn name = new TableColumn("NAME");
-        name.setCellValueFactory(new PropertyValueFactory("name"));
-        TableColumn employed = new TableColumn("EMPLOYED");
-        employed.setCellValueFactory(new PropertyValueFactory("isEmployed"));
-        tableView.getColumns().addAll(id, name, employed);
-
-        dataContainer.getChildren().add(tableView);
-    }
-
-    public void addPerson(Person person) {
-        //masterData.add(new NewPersonViewModel(person));
-        //tableView.refresh();
     }
 
     private void loadData() {
         String searchText = searchField.getText();
-/*
-        Task<ObservableList<NewPersonViewModel>> task = new Task<>() {
-            @Override
-            protected ObservableList<NewPersonViewModel> call() throws Exception {
-                updateMessage("Loading data");
-                return FXCollections.observableArrayList(masterData
-                        .stream()
-                        .filter(value -> value.getName().toLowerCase().contains(searchText))
-                        .collect(Collectors.toList()));
-            }
-        };
 
-        task.setOnSucceeded(event -> {
-            results = task.getValue();
-            tableView.setItems(FXCollections.observableList(results));
-        });
-
-        Thread th = new Thread(task);
-        th.setDaemon(true);
-        th.start();
-*/
+        personListController.filterList(searchText);
     }
 
 }
--- src/main/java/at/fhtw/swen2/tutorial/presentation/viewmodel/PersonListViewModel.java
+++ src/main/java/at/fhtw/swen2/tutorial/presentation/viewmodel/PersonListViewModel.java
@@ -1,12 +1,19 @@
 package at.fhtw.swen2.tutorial.presentation.viewmodel;
 
+import at.fhtw.swen2.tutorial.service.PersonService;
 import at.fhtw.swen2.tutorial.service.model.Person;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class PersonListViewModel {
+
+    @Autowired
+    PersonService personService;
 
     private ObservableList<NewPersonViewModel> personListItems = FXCollections.observableArrayList();
 
@@ -17,6 +24,16 @@ public class PersonListViewModel {
     public void addItem(Person person) {
         personListItems.add(new NewPersonViewModel(person));
     }
+
+    public void clearItems(){ personListItems.clear(); }
+
+    public void initList(){
+        personService.getPersonList().forEach(p -> {
+            addItem(p);
+        });
+    }
+
+
 
 
 }
